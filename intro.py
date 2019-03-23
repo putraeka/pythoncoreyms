4750e16af2d405f5ebe8d2392a28292eb213112f
putraeka <gh@eka.gdn>
2019-03-23 23:43:18 +0700

    fungsi lower & upper

--- intro.py
+++ intro.py
@@ -1,2 +1,9 @@
-message ="Helo world"
-print(message[0])
+#message ="Helo world"
+#print(message[0])
+
+#print tulisan jadi lowercase
+#message = "Helo world"
+#print(message.lower())
+
+message = "Helo world"
+print(message.upper())
