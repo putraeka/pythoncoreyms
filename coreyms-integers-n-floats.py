d5fd2950305d4fe9227281a88e2110a1c5b4c05d
putraeka <gh@eka.gdn>
2019-05-12 11:51:01 +0700

    video 3 Integers and Floats - Working with Numeric Data

---
+++ coreyms-integers-n-floats.py
@@ -0,0 +1,31 @@
+# Untuk mengetahui tipe dari sebuah variable bisa menggunakan perintah
+# print(type(variable))
+
+# contoh
+
+num = 3
+numf = 3.14
+numneg = -3
+
+print(type(num))
+print(type(numf))
+print(abs(numneg)) #fungsi dari abs adalah membuat negative value menjadi positive
+print(round(numf)) #fungsi dari round adalah membulatkan bilangan decimal
+print(round(numf, 1)) #fungsi round juga bisa diberi argument, misalnya seperti contoh diatas. Pembulatan dari variable num menjadi 3.2 alih-alih menjadi 3 (pembulatan kebawah)
+
+# Arithmetic Operators :
+# Addition :        3 + 2
+# Substraction :    3 - 2
+# Multiplication :  3 * 2
+# Division :        3 / 2
+# Floor Division :  3 // 2
+# Exponent :        3 ** 2
+# Modulus :         3 % 2
+
+# Comparisons :
+# Equal :           3 == 2
+# Not Equal :       3 != 2
+# Greater Than :    3 > 2
+# Less Than :       3 < 2
+# Greater or Equal :3 >= 2
+# Less or Equal :   3 <= 2
